72a9157ec1da351be8fc43d13b940ce8e6ae5f00
abredagn <abredagn@gmail.com>
2025-07-24 12:32:39 +0300

    users api basic implementation

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { use } from 'passport';
+
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ){}
+
+    async create(createUserDto: CreateUserDto): Promise<any> {
+        const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+        const user = this.userRepository.create({
+            ...CreateUserDto,
+            password: hashedPassword,
+            isEmailVerified: false
+        });
+        return this.userRepository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.userRepository.findOneBy({email});
+    }
+
+    async verifyEmail(userId: number): Promise<void> {
+        await this.userRepository.update(userId, {isEmailVerified: true});
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+}
