c5aa21f8673d2dff313436c718c8dc92756f002e
abredagn <abredagn@gmail.com>
2025-07-28 00:09:10 +0300

    all done on the board api

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { Board } from './entities/board.entity';
+import { Project } from 'src/projects/entities/project.entity';
+import { User } from 'src/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board, Project, User])],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
 export class BoardsModule {}
+
+
