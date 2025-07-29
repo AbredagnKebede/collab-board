5401555470a63bdb9a484ea4fdfaec42eaf2bd64
abredagn <abredagn@gmail.com>
2025-07-29 22:55:24 +0300

    list http gate done.

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { ListsService } from './lists.service';
+import { CreateListDto } from './dto/create-list.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateListDto } from './dto/update-list.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('lists')
-export class ListsController {}
+export class ListsController {
+    constructor(private readonly listsService: ListsService) {}
+
+    @Post()
+    async create(@Body() createListDto: CreateListDto) {
+        return this.listsService.create(createListDto);
+    }
+
+    @Get()
+    async findAll() { 
+        return this.listsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.listsService.findOne(id);
+    }
+
+    @Patch(':id')
+    async update(@Param('id', ParseIntPipe) id: number ,@Body() updateListDto: UpdateListDto) {
+        return this.listsService.update(id, updateListDto);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number){
+        return this.listsService.remove(id);    
+    }
+}
--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from 'src/boards/entities/board.entity';
+import { List } from './entities/list.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board, List])],
   controllers: [ListsController],
   providers: [ListsService]
 })
