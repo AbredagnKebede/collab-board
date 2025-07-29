36eaa942699457b5fce0b86c00f28e4ee550d0c2
abredagn <abredagn@gmail.com>
2025-07-30 00:20:05 +0300

    all set of the cards endpoints

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CardsService } from './cards.service';
+import { CreateCardDto } from './dto/create-card.dto';
+import { UpdateCardDto } from './dto/update-card.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('cards')
-export class CardsController {}
+export class CardsController {
+    constructor(private readonly cardsService: CardsService) {}
+
+    @Post()
+    async create(@Body() createCardDto: CreateCardDto) {
+        return this.cardsService.create(createCardDto);
+    }
+
+    @Get()
+    async findAll() { 
+        return this.cardsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.cardsService.findOne(id);
+    }
+
+    @Patch(':id')
+    async update(@Param('id', ParseIntPipe) id: number, @Body() updateCardDto: UpdateCardDto) {
+        return this.cardsService.update(id, updateCardDto);
+    }
+
+    @Delete(':id')  
+    async remove(@Param('id', ParseIntPipe) id: number) {
+        return this.cardsService.remove(id)
+    }
+}
--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { List } from 'src/lists/entities/list.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Card } from './entities/card.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Card, User, List])],
   controllers: [CardsController],
   providers: [CardsService]
 })
