9f8d63bf8fac6572faae10062f88c0034ac0dc93
abredagn <abredagn@gmail.com>
2025-07-30 10:13:33 +0300

    entity and other modification

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,5 +1,21 @@
-import { PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Card } from 'src/cards/entities/card.entity';
+import { User } from 'src/users/entities/user.entity';
 
-export class Comment{
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  content: string;
+
+  @ManyToOne(() => Card, (card) => card.comments, { onDelete: 'CASCADE' })
+  card: Card;
+
+  @ManyToOne(() => User)
+  author: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
