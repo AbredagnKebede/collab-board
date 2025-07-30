bd35f61f30ba7270bb0af6d5392fcb6ea3a13334
abredagn <abredagn@gmail.com>
2025-07-30 10:28:18 +0300

    comment endpoints are done

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Body, Param, ParseIntPipe, Patch, Get, Delete} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { User } from 'src/users/entities/user.entity';
+import { Request } from 'express';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+    constructor(private readonly commentsService: CommentsService) {}
+
+    @Post()
+    async create(@Body() createCommentDto: CreateCommentDto, @Req() req: Request) {
+        const user = req.user as User;
+        return this.commentsService.create(createCommentDto, user);
+    }
+
+    @Get('card/:cardId')
+    async findByCard(@Param('id', ParseIntPipe) id: number) {
+        return this.commentsService.findByCard(id);
+    }
+
+    @Patch(':id')
+    async update(@Body() updateCommentDto: UpdateCommentDto, @Param('id', ParseIntPipe) id: number) {
+        return this.commentsService.update(id, updateCommentDto);
+    }
+
+    @Delete(':id') 
+    async remove(@Param('id', ParseIntPipe) id: number) {
+        return this.commentsService.remove(id);
+    }
+}
